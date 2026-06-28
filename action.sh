19384ea3899c49c21d5c77e76c489ecb5a826085
Hecker <aliking66t@gmail.com>
2026-06-28 11:51:36 +0500

    Remove duplicate module files from root

--- action.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-am start -a android.intent.action.VIEW -d "https://t.me/pixel4seriesupdates" >/dev/null 2>&1
--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-resetprop --delete persist.sys.refresh_rate.override 2>/dev/null
-settings delete system peak_refresh_rate
-settings delete system min_refresh_rate
-device_config delete display_manager peak_refresh_rate
-device_config delete display_manager min_refresh_rate
-settings delete global battery_saver_constants 2>/dev/null
-settings delete global low_power_refresh_rate 2>/dev/null
-rm -rf /data/adb/smoothdisplay
